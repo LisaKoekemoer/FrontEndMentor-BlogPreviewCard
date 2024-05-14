3098df261ce3f0343c5d3a2a461759e6c152bb3f
Alicia <alicia.koekemoer98@gmail.com>
2024-05-14 21:01:14 +0200

    Finalised Blog Preview Card Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import illustration from './images/illustration-article.svg';
+import Greg from './images/image-avatar.webp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='learning-card'>
+
+          <img className='illustration' src={illustration} alt=""/>  
+
+          <div className='learning-tab'>
+            <p>Learning</p>
+          </div>
+
+          <p>Published 21 Dec 2023</p>
+
+          <h2>HTML & CSS foundations</h2>
+
+          <p>These languages are the backbone of every website, defining structure, content, and presentation.</p>
+
+          <div className='greg-cont'>
+            <img className='Greg' src={Greg} alt="Greg Hooper"/>
+            <p>Greg Hooper</p>
+          </div>
+
+      </div>
+    </>
   );
 }
 
